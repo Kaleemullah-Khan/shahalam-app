28f0581da382d22cf90e9c57151cee37c39b0064
Kaleemullah-Khan <Kaleempathan567@gmail.com>
2021-02-13 01:32:16 +0530

    night practice

--- src/Component/LeftBodySection/Menus.jsx
+++ src/Component/LeftBodySection/Menus.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
-import {ListGroup, ListGroupItem} from "react-bootstrap"
+import {ListGroup, ListGroupItem} from "react-bootstrap";
+import './Menus.css';
 const Menus = () => {
     return (
         <div>
-            <ListGroup>
-  <ListGroup.Item>CSE</ListGroup.Item>
-  <ListGroup.Item >CIVIL</ListGroup.Item>
-  <ListGroup.Item >MECHANICAL</ListGroup.Item>
-  <ListGroup.Item >ECE</ListGroup.Item>
-  <ListGroup.Item >EEE</ListGroup.Item>
-  <ListGroup.Item >EXTENSION ACTIVITIES</ListGroup.Item>
-  <ListGroup.Item >INTERSHIP PROGRAM</ListGroup.Item>
-  <ListGroup.Item >CERTIFICATION PROGRAM</ListGroup.Item>
-  <ListGroup.Item >CERTIFICATION COURSES</ListGroup.Item>
-  <ListGroup.Item >PARIKSHA PE CHARCHA 2.0</ListGroup.Item>
-  <ListGroup.Item >FACULTY DEVELOPMENT PROGRAM</ListGroup.Item>
-</ListGroup>
+            <ListGroup className="list">
+  <ListGroup.Item className="btn">CSE</ListGroup.Item>
+  <ListGroup.Item className="btn">CIVIL</ListGroup.Item>
+  <ListGroup.Item className="btn">MECHANICAL</ListGroup.Item>
+  <ListGroup.Item className="btn">ECE</ListGroup.Item>
+  <ListGroup.Item className="btn">EEE</ListGroup.Item>
+  <ListGroup.Item className="btn">EXTENSION ACTIVITIES</ListGroup.Item>
+  <ListGroup.Item className="btn">INTERSHIP PROGRAM</ListGroup.Item>
+  <ListGroup.Item className="btn">CERTIFICATION PROGRAM</ListGroup.Item>
+  <ListGroup.Item className="btn">CERTIFICATION COURSES</ListGroup.Item>
+  <ListGroup.Item className="btn">PARIKSHA PE CHARCHA 2.0</ListGroup.Item>
+  <ListGroup.Item className="btn">FACULTY DEVELOPMENT PROGRAM</ListGroup.Item>
+</ListGroup> 
         </div>
     );
 };
