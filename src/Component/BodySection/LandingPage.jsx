cc1c422dac0f6426ae706f895dd83cde06caf813
Kaleemullah-Khan <Kaleempathan567@gmail.com>
2021-02-14 00:22:19 +0530

    cse events section part

--- src/Component/BodySection/LandingPage.jsx
+++ src/Component/BodySection/LandingPage.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Civil from '../LeftBodySection/CivilEvents/Civil'
 import Menus from '../LeftBodySection/Menus'
+import Cse from '../LeftBodySection/CseEvents/Cse'
 
 const LandingPage = () => {
     return (
         <div>
                <Menus />
                <Civil />
+               <Cse />
 
         </div>
     );
--- src/Component/LeftBodySection/CseEvents/Cse.jsx
+++ src/Component/LeftBodySection/CseEvents/Cse.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import {ListGroup, Card, Row,Col} from 'react-bootstrap'
 const Cse = () => {
     return (
         <div>
@@ -16,9 +16,19 @@ const Cse = () => {
     <ListGroup.Item>
     <Card.Link href="#">Workshop On Adobe Device Days</Card.Link>
     </ListGroup.Item>
+    <Card.Header style={{background:'lightblue',fontWeight:'bold'}}>2016-17</Card.Header>
+    <ListGroup.Item>
+    <Card.Link href="#">Workshop On Android Development</Card.Link>
+    </ListGroup.Item>
     <Card.Header style={{background:'lightblue',fontWeight:'bold'}}>2017-18</Card.Header>
     <ListGroup.Item>
-    <Card.Link href="#">Workshop On Recent Trends</Card.Link>
+    <Card.Link href="#">Workshop On Linux</Card.Link>
+    </ListGroup.Item>
+    <ListGroup.Item>
+    <Card.Link href="#">Guest Lecture On ADS and CN</Card.Link>
+    </ListGroup.Item>
+    <ListGroup.Item>
+    <Card.Link href="#">Guest Lecture On DBMS</Card.Link>
     </ListGroup.Item>
     </ListGroup>
 </Card>
