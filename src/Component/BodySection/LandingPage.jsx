865edc0361f5c4693acd71804df651da67fba4d1
Kaleemullah-Khan <Kaleempathan567@gmail.com>
2021-02-13 14:00:04 +0530

    shah alam task

--- src/Component/BodySection/LandingPage.jsx
+++ src/Component/BodySection/LandingPage.jsx
@@ -1,17 +1,12 @@
 import React from 'react';
-import {Row, Col, Card} from "react-bootstrap"
+
 import Menus from '../LeftBodySection/Menus'
 
 const LandingPage = () => {
     return (
         <div>
-            <Card>
-           <Row>
-               <Col md={4}>
                <Menus />
-               </Col>
-           </Row>
-           </Card>
+
         </div>
     );
 };
--- src/Component/LeftBodySection/Menus.jsx
+++ src/Component/LeftBodySection/Menus.jsx
@@ -1,22 +1,87 @@
 import React from 'react';
-import {ListGroup, ListGroupItem} from "react-bootstrap";
+import {ListGroup, Card, Row,Col,Carousel} from "react-bootstrap";
 import './Menus.css';
 const Menus = () => {
     return (
         <div>
-            <ListGroup className="list">
-  <ListGroup.Item className="btn">CSE</ListGroup.Item>
-  <ListGroup.Item className="btn">CIVIL</ListGroup.Item>
-  <ListGroup.Item className="btn">MECHANICAL</ListGroup.Item>
-  <ListGroup.Item className="btn">ECE</ListGroup.Item>
-  <ListGroup.Item className="btn">EEE</ListGroup.Item>
-  <ListGroup.Item className="btn">EXTENSION ACTIVITIES</ListGroup.Item>
-  <ListGroup.Item className="btn">INTERSHIP PROGRAM</ListGroup.Item>
-  <ListGroup.Item className="btn">CERTIFICATION PROGRAM</ListGroup.Item>
-  <ListGroup.Item className="btn">CERTIFICATION COURSES</ListGroup.Item>
-  <ListGroup.Item className="btn">PARIKSHA PE CHARCHA 2.0</ListGroup.Item>
-  <ListGroup.Item className="btn">FACULTY DEVELOPMENT PROGRAM</ListGroup.Item>
-</ListGroup> 
+            <Row>
+                <Col lg="2">
+        <Card style={{ width: '18rem' }}>
+  <Card.Header style={{background:'lightblue',fontWeight:'bold'}}>EVENTS</Card.Header>
+  <ListGroup variant="flush">
+    <ListGroup.Item>
+    <Card.Link href="#">CSE</Card.Link>
+    </ListGroup.Item>
+    <ListGroup.Item>
+    <Card.Link href="#">CIVIL</Card.Link>
+    </ListGroup.Item>
+    <ListGroup.Item>
+    <Card.Link href="#">MECHANICAL</Card.Link>
+    </ListGroup.Item>
+    <ListGroup.Item>
+    <Card.Link href="#">ECE</Card.Link>
+    </ListGroup.Item>
+    <ListGroup.Item>
+    <Card.Link href="#">EEE</Card.Link>
+    </ListGroup.Item>
+    <ListGroup.Item>
+    <Card.Link href="#">EXTENTION ACTIVITY</Card.Link>
+    </ListGroup.Item>
+    <ListGroup.Item>
+    <Card.Link href="#">INTERNSHIP PROGRAM</Card.Link>
+    </ListGroup.Item>
+    <ListGroup.Item>
+    <Card.Link href="#">CERTIFICATION PROGRAM</Card.Link>
+    </ListGroup.Item>
+    <ListGroup.Item>
+    <Card.Link href="#">CERTIFICATE COURSES</Card.Link>
+    </ListGroup.Item>
+    <ListGroup.Item>
+    <Card.Link href="#">PARIKSHA PE CHARCHA 2.0</Card.Link>
+    </ListGroup.Item>
+    <ListGroup.Item>
+    <Card.Link href="#">FACULTY DEVELOPMENT PROGRAM</Card.Link>
+    </ListGroup.Item>
+  </ListGroup>
+</Card>
+</Col>
+<Col lg="8">
+<Carousel>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="Assets/img4.jpg"
+      alt="First slide"
+    />
+    <Carousel.Caption>
+   
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="Assets/img5.jpg"
+      alt="Third slide"
+    />
+
+    <Carousel.Caption>
+      
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="Assets/img2.jpg"
+      alt="Third slide"
+    />
+
+    <Carousel.Caption>
+      
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel>
+</Col>
+</Row>
         </div>
     );
 };
