90bcea8cd0e617b0be5a8f43b47f38ac30ed48be
Kaleemullah-Khan <Kaleempathan567@gmail.com>
2021-02-15 12:41:13 +0530

    eventsa

--- src/Component/LeftBodySection/ExtentionActivityEvents/ActivityEvents.jsx
+++ src/Component/LeftBodySection/ExtentionActivityEvents/ActivityEvents.jsx
@@ -1,1 +1,13 @@
-r
+import React from 'react';
+import {Button} from 'react-bootstrap'
+const ActivityEvents = () => {
+    return (
+        <div>
+              <Button variant="primary" href="pdf/extension activities.pdf">
+    Block level button
+  </Button>
+        </div>
+    );
+};
+
+export default ActivityEvents;
